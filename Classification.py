d28e0ddea7b29925a59720976abce76f58630815
chourasiahemanshu <chourasiahemanshu@gmail.com>
2020-04-26 23:45:39 -0400

    Updated code as we discussed

--- Classification.py
+++ Classification.py
@@ -58,6 +58,7 @@ def processTheList(fileList):
     total_ham_word_count = 0
     total_ham_files =0
     total_spam_files = 0
+    vocab = {}
     for x in fileList:
         f = open(trainingDataPath+"/"+x, "r")
         # f = open(trainingDataPath+"/"+"train-ham-00001.txt", "r")
@@ -65,74 +66,64 @@ def processTheList(fileList):
         words = preprocess_string(aString)
         listOfWords = re.split(" ", words)
         className = re.split("-",x)[1]
-        vocab = {}
         word_count = 0
-        for word in listOfWords:
-            word_count = word_count+1
-            if word in vocab:
-                count = vocab.get(word)
-                count = count + 1
-                vocab[word] = count
-            else:
-                vocab[word] = 1
+
+        if className == "ham":
+            for word in listOfWords:
+                word_count = word_count + 1
+                if word in vocab:
+                    count = vocab.get(word)[0]
+                    count = count+1
+                    vocab[word][0] = count
+                else:
+                    vocab[word] = [1,0]
+        else:
+            for word in listOfWords:
+                word_count = word_count + 1
+                if word in vocab:
+                    count = vocab.get(word)[1]
+                    count = count + 1
+                    vocab[word][1] = count
+                else:
+                    vocab[word] = [0,1]
+
         if className == "ham":
             total_ham_files = total_ham_files+1
-            total_ham_word_count = total_ham_word_count +word_count
+            total_ham_word_count = total_ham_word_count + word_count
         else:
             total_spam_files = total_spam_files + 1
             total_spam_word_count = total_spam_word_count + word_count
-
-        return vocab , total_spam_word_count , total_ham_word_count, total_spam_files , total_ham_files
-
-
-        # print(ham_vocab)
-        # print ("words are ",listOfWords)
+    # print(vocab)
+    return vocab , total_spam_word_count , total_ham_word_count, total_spam_files , total_ham_files
 
 
-def calculateTotalWords(spam,ham):
-    spamCount = 0
-    hamCount = 0
-    for a in spam:
-        spamCount =  spamCount + spam.get(a)
 
-    for b in ham:
-        hamCount = hamCount + ham.get(b)
-
-    return spamCount,hamCount
-
-
-def calculateConditionalProb(count, vocab):
+def calculateConditionalProb(ham_count, spam_count,vocab):
     cond_prob_vocab = {}
-    for a in vocab:
-        cond_prob_vocab[a]=vocab.get(a)/count
+    sorted_vocab_keys = sorted (vocab.keys())
+    for a in  sorted_vocab_keys:
+        ham_cond_prob=vocab.get(a)[0]/ham_count
+        spam_cond_prob=vocab.get(a)[1]/spam_count
+        cond_prob_vocab[a]=[ham_cond_prob,spam_cond_prob]
     return cond_prob_vocab
 
-def calculateSmoothConditionalProb(count, vocab):
-    cond_prob_vocab = {}
-    for a in vocab:
-        # Todo: need to verify the formula after smoothing
-        cond_prob_vocab[a]=vocab.get(a)+0.5/count+count*0.5
-    return cond_prob_vocab
-
-# def printTrainingData():
+#         # Todo: need to verify the formula after smoothing
+#         cond_prob_vocab[a]=vocab.get(a)+0.5/count+count*0.5
 
 
-# Todo : Can i store my vocab in numpy array
-spam_vocab = {}
-ham_vocab = {}
+
+
+
 # Exctracting files
 listOfFiles = importTrainingFiles()
 # processing files and cresting the vocabulary
+
 vocabulary , spam_word_count , ham_word_count , spam_file_count , ham_file_count = processTheList(listOfFiles)
-# count the total words in each vocab
-# spamCount, hamCount = calculateTotalWords(spam_vocab,ham_vocab)
 
-conditionalSpamProb = calculateConditionalProb(spam_word_count,spam_vocab)
-conditionalHamProb = calculateConditionalProb(ham_word_count,ham_vocab)
+# vocabulary is dictionary: with word as key  and value is a [ham_word_count , spam_word_count]
+print(vocabulary)
+conditionalProb = calculateConditionalProb(ham_word_count,spam_word_count,vocabulary)
 
-# smoothing
-conditionalSpamSmoothProb = calculateSmoothConditionalProb(spam_word_count,vocabulary)
-conditionalHamSmoothProb = calculateSmoothConditionalProb(ham_word_count,vocabulary)
 #todo: showing result in sorted manner
 
 #Todo: testing
